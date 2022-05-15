13e60c17cec3620eb58477a7d3c8b574078ebc4e
pranab9732894841 <pranabesh.s@sovtechnologies.com>
2022-05-15 22:40:53 +0530

    add two new class
    add two new class

---
+++ lib/Appointment.dart
@@ -0,0 +1,173 @@
+import 'dart:convert';
+
+class Appointment {
+  String userId;
+  String doctorId;
+  String clinicId;
+  String? scheduleId;
+  String createDate;
+  int epoch;
+  String status;
+  String? bookingDate;
+  int? lastActiveEpoch;
+  String? reason;
+  String? note;
+  String? location;
+  String? patientName;
+  String? patientId;
+  Appointment({
+    required this.userId,
+    required this.doctorId,
+    required this.clinicId,
+    this.scheduleId,
+    required this.createDate,
+    required this.epoch,
+    required this.status,
+    this.bookingDate,
+    this.lastActiveEpoch,
+    this.reason,
+    this.note,
+    this.location,
+    this.patientName,
+    this.patientId,
+  });
+
+  Appointment copyWith({
+    String? userId,
+    String? doctorId,
+    String? clinicId,
+    String? scheduleId,
+    String? createDate,
+    int? epoch,
+    String? status,
+    String? bookingDate,
+    int? lastActiveEpoch,
+    String? reason,
+    String? note,
+    String? location,
+    String? patientName,
+    String? patientId,
+  }) {
+    return Appointment(
+      userId: userId ?? this.userId,
+      doctorId: doctorId ?? this.doctorId,
+      clinicId: clinicId ?? this.clinicId,
+      scheduleId: scheduleId ?? this.scheduleId,
+      createDate: createDate ?? this.createDate,
+      epoch: epoch ?? this.epoch,
+      status: status ?? this.status,
+      bookingDate: bookingDate ?? this.bookingDate,
+      lastActiveEpoch: lastActiveEpoch ?? this.lastActiveEpoch,
+      reason: reason ?? this.reason,
+      note: note ?? this.note,
+      location: location ?? this.location,
+      patientName: patientName ?? this.patientName,
+      patientId: patientId ?? this.patientId,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    final result = <String, dynamic>{};
+
+    result.addAll({'userId': userId});
+    result.addAll({'doctorId': doctorId});
+    result.addAll({'clinicId': clinicId});
+    if (scheduleId != null) {
+      result.addAll({'scheduleId': scheduleId});
+    }
+    result.addAll({'createDate': createDate});
+    result.addAll({'epoch': epoch});
+    result.addAll({'status': status});
+    if (bookingDate != null) {
+      result.addAll({'bookingDate': bookingDate});
+    }
+    if (lastActiveEpoch != null) {
+      result.addAll({'lastActiveEpoch': lastActiveEpoch});
+    }
+    if (reason != null) {
+      result.addAll({'reason': reason});
+    }
+    if (note != null) {
+      result.addAll({'note': note});
+    }
+    if (location != null) {
+      result.addAll({'location': location});
+    }
+    if (patientName != null) {
+      result.addAll({'patientName': patientName});
+    }
+    if (patientId != null) {
+      result.addAll({'patientId': patientId});
+    }
+
+    return result;
+  }
+
+  factory Appointment.fromMap(Map<String, dynamic> map) {
+    return Appointment(
+      userId: map['userId'] ?? '',
+      doctorId: map['doctorId'] ?? '',
+      clinicId: map['clinicId'] ?? '',
+      scheduleId: map['scheduleId'],
+      createDate: map['createDate'] ?? '',
+      epoch: map['epoch']?.toInt() ?? 0,
+      status: map['status'] ?? '',
+      bookingDate: map['bookingDate'],
+      lastActiveEpoch: map['lastActiveEpoch']?.toInt(),
+      reason: map['reason'],
+      note: map['note'],
+      location: map['location'],
+      patientName: map['patientName'],
+      patientId: map['patientId'],
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Appointment.fromJson(String source) =>
+      Appointment.fromMap(json.decode(source));
+
+  @override
+  String toString() {
+    return 'Appointment(userId: $userId, doctorId: $doctorId, clinicId: $clinicId, scheduleId: $scheduleId, createDate: $createDate, epoch: $epoch, status: $status, bookingDate: $bookingDate, lastActiveEpoch: $lastActiveEpoch, reason: $reason, note: $note, location: $location, patientName: $patientName, patientId: $patientId)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is Appointment &&
+        other.userId == userId &&
+        other.doctorId == doctorId &&
+        other.clinicId == clinicId &&
+        other.scheduleId == scheduleId &&
+        other.createDate == createDate &&
+        other.epoch == epoch &&
+        other.status == status &&
+        other.bookingDate == bookingDate &&
+        other.lastActiveEpoch == lastActiveEpoch &&
+        other.reason == reason &&
+        other.note == note &&
+        other.location == location &&
+        other.patientName == patientName &&
+        other.patientId == patientId;
+  }
+
+  @override
+  int get hashCode {
+    return userId.hashCode ^
+        doctorId.hashCode ^
+        clinicId.hashCode ^
+        scheduleId.hashCode ^
+        createDate.hashCode ^
+        epoch.hashCode ^
+        status.hashCode ^
+        bookingDate.hashCode ^
+        lastActiveEpoch.hashCode ^
+        reason.hashCode ^
+        note.hashCode ^
+        location.hashCode ^
+        patientName.hashCode ^
+        patientId.hashCode;
+  }
+}
---
+++ lib/Patient.dart
@@ -0,0 +1,209 @@
+import 'dart:convert';
+
+class Patient {
+  String patientName;
+  String? patientId;
+  String patientPhone;
+  String? patientEmail;
+  String? patientAddress;
+  String patientGender;
+  String? patientDob;
+  String? patientAge;
+  String? patientBloodGroup;
+  String? patientAllergy;
+  String? patientHistory;
+  String? patientWeight;
+  String? patientHeight;
+  String? patientBmi;
+  String? patientBp;
+  String? patientPulse;
+  String? patientImage;
+  Patient({
+    required this.patientName,
+    this.patientId,
+    required this.patientPhone,
+    this.patientEmail,
+    this.patientAddress,
+    required this.patientGender,
+    this.patientDob,
+    this.patientAge,
+    this.patientBloodGroup,
+    this.patientAllergy,
+    this.patientHistory,
+    this.patientWeight,
+    this.patientHeight,
+    this.patientBmi,
+    this.patientBp,
+    this.patientPulse,
+    this.patientImage,
+  });
+
+  Patient copyWith({
+    String? patientName,
+    String? patientId,
+    String? patientPhone,
+    String? patientEmail,
+    String? patientAddress,
+    String? patientGender,
+    String? patientDob,
+    String? patientAge,
+    String? patientBloodGroup,
+    String? patientAllergy,
+    String? patientHistory,
+    String? patientWeight,
+    String? patientHeight,
+    String? patientBmi,
+    String? patientBp,
+    String? patientPulse,
+    String? patientImage,
+  }) {
+    return Patient(
+      patientName: patientName ?? this.patientName,
+      patientId: patientId ?? this.patientId,
+      patientPhone: patientPhone ?? this.patientPhone,
+      patientEmail: patientEmail ?? this.patientEmail,
+      patientAddress: patientAddress ?? this.patientAddress,
+      patientGender: patientGender ?? this.patientGender,
+      patientDob: patientDob ?? this.patientDob,
+      patientAge: patientAge ?? this.patientAge,
+      patientBloodGroup: patientBloodGroup ?? this.patientBloodGroup,
+      patientAllergy: patientAllergy ?? this.patientAllergy,
+      patientHistory: patientHistory ?? this.patientHistory,
+      patientWeight: patientWeight ?? this.patientWeight,
+      patientHeight: patientHeight ?? this.patientHeight,
+      patientBmi: patientBmi ?? this.patientBmi,
+      patientBp: patientBp ?? this.patientBp,
+      patientPulse: patientPulse ?? this.patientPulse,
+      patientImage: patientImage ?? this.patientImage,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    final result = <String, dynamic>{};
+
+    result.addAll({'patientName': patientName});
+    if (patientId != null) {
+      result.addAll({'patientId': patientId});
+    }
+    result.addAll({'patientPhone': patientPhone});
+    if (patientEmail != null) {
+      result.addAll({'patientEmail': patientEmail});
+    }
+    if (patientAddress != null) {
+      result.addAll({'patientAddress': patientAddress});
+    }
+    result.addAll({'patientGender': patientGender});
+    if (patientDob != null) {
+      result.addAll({'patientDob': patientDob});
+    }
+    if (patientAge != null) {
+      result.addAll({'patientAge': patientAge});
+    }
+    if (patientBloodGroup != null) {
+      result.addAll({'patientBloodGroup': patientBloodGroup});
+    }
+    if (patientAllergy != null) {
+      result.addAll({'patientAllergy': patientAllergy});
+    }
+    if (patientHistory != null) {
+      result.addAll({'patientHistory': patientHistory});
+    }
+    if (patientWeight != null) {
+      result.addAll({'patientWeight': patientWeight});
+    }
+    if (patientHeight != null) {
+      result.addAll({'patientHeight': patientHeight});
+    }
+    if (patientBmi != null) {
+      result.addAll({'patientBmi': patientBmi});
+    }
+    if (patientBp != null) {
+      result.addAll({'patientBp': patientBp});
+    }
+    if (patientPulse != null) {
+      result.addAll({'patientPulse': patientPulse});
+    }
+    if (patientImage != null) {
+      result.addAll({'patientImage': patientImage});
+    }
+
+    return result;
+  }
+
+  factory Patient.fromMap(Map<String, dynamic> map) {
+    return Patient(
+      patientName: map['patientName'] ?? '',
+      patientId: map['patientId'],
+      patientPhone: map['patientPhone'] ?? '',
+      patientEmail: map['patientEmail'],
+      patientAddress: map['patientAddress'],
+      patientGender: map['patientGender'] ?? '',
+      patientDob: map['patientDob'],
+      patientAge: map['patientAge'],
+      patientBloodGroup: map['patientBloodGroup'],
+      patientAllergy: map['patientAllergy'],
+      patientHistory: map['patientHistory'],
+      patientWeight: map['patientWeight'],
+      patientHeight: map['patientHeight'],
+      patientBmi: map['patientBmi'],
+      patientBp: map['patientBp'],
+      patientPulse: map['patientPulse'],
+      patientImage: map['patientImage'],
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Patient.fromJson(String source) =>
+      Patient.fromMap(json.decode(source));
+
+  @override
+  String toString() {
+    return 'Patient(patientName: $patientName, patientId: $patientId, patientPhone: $patientPhone, patientEmail: $patientEmail, patientAddress: $patientAddress, patientGender: $patientGender, patientDob: $patientDob, patientAge: $patientAge, patientBloodGroup: $patientBloodGroup, patientAllergy: $patientAllergy, patientHistory: $patientHistory, patientWeight: $patientWeight, patientHeight: $patientHeight, patientBmi: $patientBmi, patientBp: $patientBp, patientPulse: $patientPulse, patientImage: $patientImage)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is Patient &&
+        other.patientName == patientName &&
+        other.patientId == patientId &&
+        other.patientPhone == patientPhone &&
+        other.patientEmail == patientEmail &&
+        other.patientAddress == patientAddress &&
+        other.patientGender == patientGender &&
+        other.patientDob == patientDob &&
+        other.patientAge == patientAge &&
+        other.patientBloodGroup == patientBloodGroup &&
+        other.patientAllergy == patientAllergy &&
+        other.patientHistory == patientHistory &&
+        other.patientWeight == patientWeight &&
+        other.patientHeight == patientHeight &&
+        other.patientBmi == patientBmi &&
+        other.patientBp == patientBp &&
+        other.patientPulse == patientPulse &&
+        other.patientImage == patientImage;
+  }
+
+  @override
+  int get hashCode {
+    return patientName.hashCode ^
+        patientId.hashCode ^
+        patientPhone.hashCode ^
+        patientEmail.hashCode ^
+        patientAddress.hashCode ^
+        patientGender.hashCode ^
+        patientDob.hashCode ^
+        patientAge.hashCode ^
+        patientBloodGroup.hashCode ^
+        patientAllergy.hashCode ^
+        patientHistory.hashCode ^
+        patientWeight.hashCode ^
+        patientHeight.hashCode ^
+        patientBmi.hashCode ^
+        patientBp.hashCode ^
+        patientPulse.hashCode ^
+        patientImage.hashCode;
+  }
+}
